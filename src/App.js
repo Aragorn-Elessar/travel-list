af9d24bb4cf84d1eb3eb1ed12b3aafab8030bf3c
Aragorn Elessar <mahmoud_gadallah@outlook.com>
2024-01-24 13:18:00 +0200

    feat: Render list items

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import { useState } from 'react';
+
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: true },
+  { id: 3, description: 'Charger', quantity: 1, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -22,7 +30,29 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map(item => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  const [isPacked, setIsPacked] = useState(item.packed);
+
+  return (
+    <li>
+      <input type="checkbox" />
+      <span
+        style={isPacked ? { textDecoration: `line-through` } : {}}
+      >{`${item.quantity} ${item.description}`}</span>
+      <button onClick={() => setIsPacked(() => !isPacked)}>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
